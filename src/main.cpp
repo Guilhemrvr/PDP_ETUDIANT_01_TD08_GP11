7dab6418fb1607d4d45c34fc113a11c0a9a1d575
Guilhem Rouviere <guilhem.rouv@gmail.com>
2023-01-17 11:21:50 +0100

    etape_3

--- src/main.cpp
+++ src/main.cpp
@@ -71,27 +71,5 @@ void setup() {
 }
 
 void loop() {
-  // // Delay between measurements.
-  // delay(delayMS);
-  // // Get temperature event and print its value.
-  // sensors_event_t event;
-  // dht.temperature().getEvent(&event);
-  // if (isnan(event.temperature)) {
-  //   Serial.println(F("Error reading temperature!"));
-  // }
-  // else {
-  //   Serial.print(F("Temperature: "));
-  //   Serial.print(event.temperature);
-  //   Serial.println(F("°C"));
-  // }
-  // // Get humidity event and print its value.
-  // dht.humidity().getEvent(&event);
-  // if (isnan(event.relative_humidity)) {
-  //   Serial.println(F("Error reading humidity!"));
-  // }
-  // else {
-  //   Serial.print(F("Humidity: "));
-  //   Serial.print(event.relative_humidity);
-  //   Serial.println(F("%"));
-  // }
+  
 }
